ad976df1cb3240ccff5a5bb458c95f501e566ef6
daxander <davidfeimer@gmail.com>
2016-01-30 07:47:23 -0800

    set main.jsx and index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var List = require('./components/List.jsx')
+var ListManager = require('./components/ListManager.jsx');
+
+ReactDOM.render(<ListManager title="List" />, document.getElementById('list'));
 
